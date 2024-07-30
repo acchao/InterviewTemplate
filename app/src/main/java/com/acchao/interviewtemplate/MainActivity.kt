37c5353cee89bf70491eda80b2c8062322de8cad
Andrew Chao <andrew.c.chao@gmail.com>
2024-07-30 10:10:27 -0700

    Add scaffolding
    adding scaffolding so we're not inside the status bar space

--- app/src/main/java/com/acchao/interviewtemplate/MainActivity.kt
+++ app/src/main/java/com/acchao/interviewtemplate/MainActivity.kt
@@ -6,8 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.TopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -16,12 +19,23 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             InterviewTemplateTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = { Text("Interview Template") },
+                            windowInsets = TopAppBarDefaults.windowInsets,
+                            contentColor = TopAppBarDefaults.topAppBarColors().titleContentColor,
+                            backgroundColor = TopAppBarDefaults.topAppBarColors().containerColor
+                        )
+                    },
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
                     Greeting(
                         name = "Andrew",
                         modifier = Modifier.padding(innerPadding)
@@ -35,7 +49,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Welcome to $name's Barebones interview template",
+        text = "$name's interview template",
         modifier = modifier
     )
 }
